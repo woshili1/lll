500c7402d897d8e34d3121cd1d9fb1241f774a48
woshili1 <45116701@qq.com>
2015-05-07 23:17:43 +0800

    3333

---
+++ mall/src/com/mall/BaseAction.java
@@ -0,0 +1,12 @@
+package com.mall;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BaseAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3766131931876234461L;
+
+}
